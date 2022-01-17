ed57709110f5b57697deb82f89ba7e32376dc130
Ghadeer <ghaora@gmail.com>
2022-01-17 16:14:43 +0200

    first commit

---
+++ myscript.js
@@ -0,0 +1,42 @@
+let app = new Vue({
+    el: "#app",
+    data: {
+        value: "add a task",
+        task:'',
+        
+        tasks: [
+            {
+                text:'Recruiting blog post',
+                done:false
+                
+            },
+            {
+                text:'Mobile app luanch',
+                done:false
+                
+            },
+            {
+                text:'Summit update to mobiile storefronts',
+                done:false
+                   
+            },
+            {
+                text:'Schedule meeting with Alex',
+                done:false
+                
+            }
+        ]
+    },
+    methods:{
+
+        addTask: function(){
+            if(this.task.length === 0)return;
+            this.tasks.push({
+                text: this.task,
+                
+            })
+        }
+    }
+   
+
+})
